252274ac59ed877d1cc2370436ec09e196a705e8
Jade <notjade.213@gmail.com>
2025-06-19 11:03:55 +0200

    init commit

---
+++ src/main/java/notjade/languages/Languages.java
@@ -0,0 +1,17 @@
+package notjade.languages;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Languages extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
